e74eb97215950e4bb5d47123e9a521f99fe59b54
Daniel Osipishin <danielos@retarus.de>
2023-02-01 15:51:13 +0100

    sofle simpleton initial

---
+++ keyboards/sofle/keymaps/simpleton/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define USB_POLLING_INTERVAL_MS 1
+#undef DEBOUNCE
+#define DEBOUNCE 3
+
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX, UNICODE_MODE_WINCOMPOSE
+
+// Don't remember the last used mode, always start with the first
+#define UNICODE_CYCLE_PERSIST false
