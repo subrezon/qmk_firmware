993843efbd88b85e919e84a0add9a20dc67f17ea
Daniel Osipishin <danielos@retarus.de>
2023-06-07 16:44:15 +0200

    sweep + sofle back to 36

--- keyboards/sofle/keymaps/simpleton/rules.mk
+++ keyboards/sofle/keymaps/simpleton/rules.mk
@@ -1,15 +1,20 @@
 CONVERT_TO = kb2040
 
-OLED_ENABLE = no
-OLED_DRIVER = SSD1306
+# hardware
+OLED_ENABLE    = no
+OLED_DRIVER    = SSD1306
 ENCODER_ENABLE = no
-LTO_ENABLE = yes
+LTO_ENABLE     = yes
 
-CONSOLE_ENABLE = no
-EXTRAKEY_ENABLE = yes
-UNICODEMAP_ENABLE = yes
+# debounce
+DEBOUNCE_TYPE = asym_eager_defer_pk
+
+# keycodes
+EXTRAKEY_ENABLE     = yes
+MOUSEKEY_ENABLE     = yes
+UNICODEMAP_ENABLE   = yes
 KEY_OVERRIDE_ENABLE = yes
 
+# debug
 BOOTMAGIC_ENABLE = yes
-
-DEBOUNCE_TYPE = asym_eager_defer_pk
+CONSOLE_ENABLE   = no
